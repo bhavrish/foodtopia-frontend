3b2240e84dda5826cff9b0e565e160c9287a6b1f
YihuiAndre <yihuiawuchen@gmail.com>
2020-10-16 16:52:31 -0400

    updated navigation bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    link: {
+        display: 'flex',
+        alignItems: 'center',
+        color: "black",
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    title: {
+        color: "black",
+    },
+}));
 
 function Nav() {
+    const classes = useStyles();
+
     return(
-        <nav> 
-            <h2> Restaurant App Name </h2>
-            <Link to="/signin"> 
-                <h3> Sign in</h3>
-            </Link> 
-        </nav>
+        <AppBar position="relative">
+            <CssBaseline />
+            <Toolbar className={classes.toolbar}>
+                <Link href="/" className={classes.title}>
+                    <Typography component="h1" variant="h5">
+                        Restaurant App Name
+                    </Typography>
+                </Link>
+                <Link href="/signin" className={classes.link}>
+                    Sign in
+                    <ExitToAppIcon />
+                </Link>
+            </Toolbar>
+        </AppBar>
     );
 }
- 
+
 export default Nav;
