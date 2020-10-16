f145fe2eae6ccd79524419610d6be9cf7521cdee
YihuiAndre <yihuiawuchen@gmail.com>
2020-10-16 08:33:13 -0400

    create color theme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import theme from './theme.js';
+import { ThemeProvider } from "@material-ui/core";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/theme.js
+++ src/theme.js
@@ -4,10 +4,15 @@ const theme = createMuiTheme({
   palette: {
     type: 'light', // light or dark mode
     background: {
-      default: '', // add background color hex code
+      default: '#f6f4e6', // add background color hex code
     },
     primary: {
-      main: '', // add primary hex code
+      main: "#e09465",
+      contrastText: '#000',
+    },
+    secondary: {
+      main: "#f1d07b",
+      contrastText: '#000',
     },
   },
 
