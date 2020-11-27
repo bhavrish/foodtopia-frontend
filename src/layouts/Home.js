3393e3cde90f090706fb77108d02a5859e908166
YihuiAndre <yihuiawuchen@gmail.com>
2020-11-27 02:00:15 -0500

    set up template of Home and Menus page

---
+++ src/layouts/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Nav} from '../components';
+
+export default function Home() {
+    return(
+        <div>
+            <Nav />
+            <h1> This is home page </h1>
+        </div>
+    );
+}
