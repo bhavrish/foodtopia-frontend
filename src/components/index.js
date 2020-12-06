50ea80a8647a5404957ee67dc5644e36988c86e2
VictoriaXY6 <xingxingyang598@yahoo.com>
2020-12-05 22:15:19 -0500

    add card components for templates

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,10 @@
 import Nav from './Nav';
 import DashboardNav from './DashboardNav';
 import DishCard from './DishCard';
+import ReviewCard from './ReviewCard';
+import ApproveCard from './ApproveCard';
+import TaskCard from './TaskCard';
+import ReceiptCard from './ReceiptCard';
+import CartCard from './CartCard';
 
-export {Nav, DashboardNav, DishCard};
+export {Nav, DashboardNav, DishCard, ReviewCard, ApproveCard, TaskCard, ReceiptCard, CartCard};
