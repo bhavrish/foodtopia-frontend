2c9f017c5d9dc74f19db904ba96b82fdc41033c9
VictoriaXY6 <xingxingyang598@yahoo.com>
2020-12-05 22:12:51 -0500

    Updated

--- src/layouts/Manager/index.js
+++ src/layouts/Manager/index.js
@@ -1,3 +1,7 @@
 import ManagListItems from './ManagListItems';
+import ManagDashboard from './ManagDashboard';
+import EmployeeList from './EmployeeList';
+import CustomerList from './CustomerList';
+import ManagDiscussion from './ManagDiscussion';
 
-export {ManagListItems};
+export {ManagListItems, ManagDashboard, EmployeeList, CustomerList, ManagDiscussion};
