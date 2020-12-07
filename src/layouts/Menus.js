4359d682fe413106b8ce60372a3c6b455040657d
zealptl <zealpatel06@gmail.com>
2020-12-07 15:05:33 -0500

    connect menuItems to server

--- src/layouts/Menus.js
+++ src/layouts/Menus.js
@@ -1,79 +1,57 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import {Nav, DishCard} from '../components';
-import HomeImg from './HomeImg.jpg';
+import { Nav, DishCard } from '../components';
+
+import MenuItemsContext from '../context/menuItems/menuItemsContext';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(1),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-    appBarSpacer: {
-        marginTop: "20px",
-    },
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  appBarSpacer: {
+    marginTop: '20px',
+  },
 }));
 
-
 export default function Menus() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    function FormRow() {
-        return (
-            <React.Fragment>
-                <Grid item xs={4}>
-                    <DishCard 
-                        imageSrc={HomeImg}
-                        title='Dish Title' 
-                        price='100'
-                        rate='2.5'
-                        description='Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica'
-                    />
-                </Grid>
-                <Grid item xs={4}>
-                    <DishCard 
-                        imageSrc={HomeImg}
-                        title='Dish Title' 
-                        price='100'
-                        rate='2.5'
-                        description='Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica'
-                    />
-                </Grid>
-                <Grid item xs={4}>
-                    <DishCard 
-                        imageSrc={HomeImg}
-                        title='Dish Title' 
-                        price='100'
-                        rate='2.5'
-                        description='Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica'
-                    />
-                </Grid>
-            </React.Fragment>
-        );
-    }
+  const menuItemsContext = useContext(MenuItemsContext);
 
-    return(
-        <div>
-            <Nav />
-            <div className={classes.appBarSpacer} />
-            <Grid container spacing={2} style={{margin: 0, width: '100%'}}>
-                <Grid container item xs={12} spacing={3}>
-                    <FormRow />
-                </Grid>
-                <Grid container item xs={12} spacing={3}>
-                    <FormRow />
-                </Grid>
-                <Grid container item xs={12} spacing={3}>
-                    <FormRow />
-                </Grid>
+  const { menuItems, getMenuItems } = menuItemsContext;
+
+  useEffect(() => {
+    getMenuItems();
+
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div>
+      <Nav />
+      <div className={classes.appBarSpacer} />
+      <Grid container spacing={2} style={{ margin: 0, width: '100%' }}>
+        <Grid container item xs={12} spacing={3}>
+          {menuItems.map((menuItem) => (
+            <Grid item xs={4}>
+              <DishCard
+                imageSrc={`http://localhost:5000/api/menuItems/images/${menuItem.image}`}
+                title={menuItem.title}
+                price={menuItem.price}
+                rate={2.5}
+                chefName={menuItem.chefName}
+                description={menuItem.description}
+              />
             </Grid>
-        </div>
-    );
+          ))}
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
