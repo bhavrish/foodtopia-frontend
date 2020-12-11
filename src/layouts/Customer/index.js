b42982633e999a3c02dc8ee5750507159d133ffe
Greg <gkimatov@gmail.com>
2020-12-11 18:06:10 -0500

    minor edits

--- src/layouts/Customer/index.js
+++ src/layouts/Customer/index.js
@@ -1,7 +1,6 @@
 import CustListItems from './CustListItems';
 import CustDashboard from './CustDashboard';
 import Cart from './Cart';
-import CustDiscussion from './CustDiscussion';
 import CustReviews from './CustReviews';
 
-export {CustListItems, CustDashboard, Cart, CustDiscussion, CustReviews};
+export {CustListItems, CustDashboard, Cart, CustReviews};
